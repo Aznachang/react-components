299d49f45dc9856e7efe8364e5c795a257b03a64
Aznachang <Aznachang@gmail.com>
2017-01-06 16:50:15 -0800

    we did it!

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// TODO
+
+//Sub-Components
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      strike: false
+    }
+  }
+
+  onListItemClick() {
+    this.setState({
+      strike: !this.state.strike
+    });
+  }
+
+  render() {
+    
+    var style = {
+      fontWeight: this.state.strike ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style = {style} onMouseOver = {this.onListItemClick.bind(this)} onMouseOut = {this.onListItemClick.bind(this)}> {this.props.item}</li>
+    );
+
+  }
+};
+
+/**Main Component**/
+var GroceryList = (props) => (
+    <ul>
+      {props.items.map(item => <GroceryListItem item = {item} />)}
+    </ul>
+);
+
+ReactDOM.render(<GroceryList items = {['milk', 'egg', 'bread']} />, document.getElementById("app"));
